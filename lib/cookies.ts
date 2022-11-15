1af488ac29d30318d21cbe426024030cad6e3c8f
Abourass <Abourassa@AssetVal.com>
2022-11-15 14:48:48 -0700

    :bulb: Clean up types

--- lib/cookies.ts
+++ lib/cookies.ts
@@ -1,26 +1,41 @@
-/**
- * Creates object for cookies manipulation on client side.
- * Allows to parse server's response in order to get cookies and compose http request to transfer cookies to the server
- * @constructor
- */
-function Cookies() {
-  this.cookies = {};
+class Cookie {
+  value: string;
+  expires?: Date;
+  secure?: boolean;
+  new?: boolean;
+
+  constructor(
+    value: string,
+    options?: { expires?: Date; secure?: boolean; new?: boolean }
+  ) {
+    this.value = value;
+    if (options) {
+      this.expires = options.expires;
+      this.secure = options.secure;
+      this.new = options.new;
+    }
+  }
 }
 
-Cookies.prototype = {
+/**
+ * A Es6 Class Version of the Cookies.js
+ */
+export default class Cookies {
+  cookies: Record<string, Cookie> = {};
+
   /**
    * Obtains value of the cookie with specified name.
    * This call checks expiration dates and does not return expired cookies.
    * @param {String} name cookie name
-   * @return {String} cookie value or null
+   * @return {String|null} cookie value or null
    */
-  get: function (name) {
+  public get(name: string) {
     const cookie = this.cookies[name];
     if (cookie && this.checkNotExpired(name)) {
       return this.cookies[name].value;
     }
     return null;
-  },
+  }
 
   /**
    * Sets cookie's value and optional options
@@ -30,85 +45,82 @@ Cookies.prototype = {
    *  - {Boolean} secure - is cookie secure or not (does not mean anything for now)
    *  - {Date} expires - cookie's expiration date. If specified then cookie will disappear after that date
    */
-  set: function (name, value, options) {
-    const cookie =
-      typeof options === 'object'
-        ? {
-            value: value,
-            expires: options.expires,
-            secure: options.secure || false,
-            new: options.new || false
-          }
-        : { value: value };
-    if (this.checkNotExpired(name, cookie)) {
-      this.cookies[name] = cookie;
+  public set(
+    name: string,
+    value: string,
+    options?: { expires: Date; secure?: boolean; new?: boolean }
+  ) {
+    if (this.checkNotExpired(name)) {
+      this.cookies[name] = new Cookie(value, options);
     }
-  },
+  }
 
   // For testing purposes
-  getExpirationDate: function (name) {
+  public getExpirationDate(name: string) {
     return this.cookies[name] ? this.cookies[name].expires : null;
-  },
+  }
 
   // Internal function
-  checkNotExpired: function (name, cookie) {
-    if (typeof cookie === 'undefined') {
-      cookie = this.cookies[name];
-    }
+  private checkNotExpired(name: string, cookie?: Cookie | any) {
+    if (typeof cookie !== 'object' || !('expires' in cookie))
+      cookie = this.cookies[name] as Cookie;
+
     const now = new Date();
-    if (cookie && cookie.expires && now > cookie.expires) {
+    if (cookie instanceof Cookie && cookie.expires && now > cookie.expires) {
       delete this.cookies[name];
       return false;
     }
     return true;
-  },
+  }
 
   /**
    * Parses headers from server's response for 'set-cookie' header and store cookie's values.
    * Also parses expiration date
    * @param headers
    */
-  parseResponse: function (headers) {
+  public parseResponse(headers: { 'set-cookie': Array<string> }) {
     const cookies = headers['set-cookie'];
-    if (cookies) {
-      cookies.forEach((c) => {
-        const cookiesParams = c.split(';');
-        const cookiePair = cookiesParams.shift().split('=');
-        const options = {};
-        cookiesParams.forEach((param) => {
-          param = param.trim();
-          if (param.toLowerCase().indexOf('expires') == 0) {
-            const date = param.split('=')[1].trim();
-            options.expires = new Date(date);
-          }
-        });
-        this.set(cookiePair[0].trim(), cookiePair[1].trim(), options);
+
+    cookies?.forEach((cookie) => {
+      const cookiesParams = cookie.split(';');
+      const cookiePair = cookiesParams.shift()?.split('=') ?? [];
+      const options: { expires?: Date } = {};
+
+      cookiesParams.forEach((param) => {
+        param = param.trim();
+        if (param.toLowerCase().indexOf('expires') === 0) {
+          const date = param.split('=')[1].trim();
+          options.expires = new Date(date);
+        }
       });
-    }
-  },
+
+      if (cookiePair.length > 0)
+        this.set(
+          cookiePair[0].trim(),
+          cookiePair[1].trim(),
+          options as { expires: Date }
+        );
+    });
+  }
 
   /**
    * Adds cookies to the provided headers as array. Does nothing if there are no cookies stored.
    * This call checks expiration dates and does not add expired cookies.
    * @param headers
    */
-  composeRequest: function (headers) {
-    if (Object.keys(this.cookies).length == 0) {
-      return;
-    }
+  public composeRequest(headers: { Cookie: undefined | string }) {
+    if (Object.keys(this.cookies).length === 0) return;
     headers.Cookie = this.toString();
-  },
+  }
 
   /**
    *
    * @return {String} cookies as 'name=value' pairs joined by semicolon
    */
-  toString: function () {
+  public toString() {
     return Object.keys(this.cookies)
       .filter(this.checkNotExpired.bind(this))
-      .map((name) => `${name}=${this.cookies[name].value}`)
+      .map(name => `${name}=${this.cookies[name].value}`)
       .join(';');
   }
-};
-
-module.exports = Cookies;
+}
